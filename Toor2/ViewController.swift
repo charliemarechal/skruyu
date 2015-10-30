5634bbf04b613fa9d02ce3ec4ce81b0640655fff
Charlie <charliemarechal@macbook-pro-de-charlie.local>
2015-10-30 00:09:55 -0400

    Initial Commit

---
+++ Toor2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toor2
+//
+//  Created by Charlie Marechal on 30/10/15.
+//  Copyright © 2015 Skruyu Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
